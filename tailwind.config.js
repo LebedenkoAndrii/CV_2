1981c4b3b5e1ec844207aac714537be3c6166537
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-03-24 00:24:56 +0200

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      height: {
+        secHeight: "650px",
+      },
+    },
+  },
+  plugins: [],
+};
