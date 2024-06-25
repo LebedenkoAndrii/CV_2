673d4c3363f3afef10da12225140032def1d53a5
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-06-25 17:30:35 +0300

    added mobile adaptive

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", function () {
   let nav = document.getElementById("nav");
+  let menu = document.getElementById("nav-btn");
   let homeSection = document.getElementById("home");
   let homeSectionHeight = homeSection.offsetHeight;
 
@@ -8,9 +9,33 @@ window.addEventListener("scroll", function () {
 
   if (window.scrollY >= homeSectionHeight) {
     nav.classList.add("fixed-nav");
+    menu.classList.add("fixed-nav-btn");
     secAbout.style.marginTop = navHeight + "px";
   } else {
     nav.classList.remove("fixed-nav");
+    menu.classList.remove("fixed-nav-btn");
     secAbout.style.marginTop = "0px";
   }
+
+  if (menu.classList.contains("open")) {
+    menu.classList.remove("open");
+    let navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("open");
+  }
+});
+
+document.getElementById("nav-btn").addEventListener("click", function () {
+  let navMenu = document.getElementById("nav-menu");
+  this.classList.toggle("open");
+  navMenu.classList.toggle("hidden");
+  navMenu.classList.toggle("flex");
+});
+
+const navItems = document.querySelectorAll("#menu-nav-item");
+navItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    let navMenu = document.getElementById("nav-menu");
+    this.classList.toggle("open");
+    navMenu.classList.toggle("hidden");
+  });
 });
